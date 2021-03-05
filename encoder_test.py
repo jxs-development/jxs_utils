64a8ff4da0ac0503b2e76973f1c4fcecb33ebd2f
CbIpok <dmitrienkomy@gmail.com>
2021-03-05 21:02:27 +0700

    fix bug

--- encoder_test.py
+++ encoder_test.py
@@ -2,29 +2,38 @@ import utilities
 import subprocess
 
 
-def encode(full_flags):
-    for full_flag in full_flags:
-        print(full_flag)
-        utilities.check_flag_is_writable(full_flag)
-        subprocess.run(full_flag)
+def encode(full_flags, index):
+    print(index)
+    utilities.check_flag_is_writable(full_flags[index])
+    subprocess.run(full_flags[index])
 
 
 def main():
     full_flags = utilities.encoder_full_flag_generator()
+    total_test = True
     for extension in full_flags:
-        for type_encoder in full_flags[extension]:
-            encode(full_flags[extension][type_encoder])
-    reference_encoded_image_list = utilities.get_encoded_image_list("tmp/reference_encoded/")
-    to_check_encoded_image_list = utilities.get_encoded_image_list("tmp/to_check_encoded/")
-    test_ok = True
-    for i in range(len(reference_encoded_image_list)):
-        if not utilities.file_cmp(reference_encoded_image_list[i], to_check_encoded_image_list[i]):
-            test_ok = False
-            break
-    if test_ok:
-        print("OK")
-    else:
-        print("BAD on", reference_encoded_image_list[i], to_check_encoded_image_list[i])
+        for index in range(len(full_flags[extension]["reference"])):
+            encode(full_flags[extension]["reference"], index)
+            encode(full_flags[extension]["to_check"], index)
+            test = utilities.file_cmp(full_flags[extension]["to_check"][index].split()[-1], full_flags[extension]["reference"][index].split()[-1])
+            print(full_flags[extension]["to_check"][index].split()[-1], full_flags[extension]["reference"][index].split()[-1],"files")
+            if test:
+                print("OK")
+            else:
+                total_test = False
+                print("BAD on",full_flags[extension]["to_check"][index].split()[-1], full_flags[extension]["reference"][index].split()[-1])
+    print("Total test:", total_test)
+    # reference_encoded_image_list = utilities.get_encoded_image_list("tmp/reference_encoded/")
+    # to_check_encoded_image_list = utilities.get_encoded_image_list("tmp/to_check_encoded/")
+    # test_ok = True
+    # for i in range(len(reference_encoded_image_list)):
+    #     if not utilities.file_cmp(reference_encoded_image_list[i], to_check_encoded_image_list[i]):
+    #         test_ok = False
+    #         break
+    # if test_ok:
+    #     print("OK")
+    # else:
+    #     print("BAD on", reference_encoded_image_list[i], to_check_encoded_image_list[i])
 
 
 if __name__ == "__main__":
