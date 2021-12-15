8f985787f18691b7a160ea968679e8e602227807
CbIpok <dmitrienkomy@gmail.com>
2021-12-15 13:12:15 +0700

    dec test is first

--- run_all.py
+++ run_all.py
@@ -1,8 +1,9 @@
 import encoder_test
 import decoder_test
 
-encoder_test_result, encoder_bad_list = encoder_test.main()
 decoder_test_result, decoder_bad_list = decoder_test.main()
+encoder_test_result, encoder_bad_list = encoder_test.main()
+
 
 print("encoder_test: ", encoder_test_result, "decoder_test: ", decoder_test_result)
 
