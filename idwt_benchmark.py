88b5725a443ef0d23de4a50ebf619cdf50ad797d
Mikhail Y. Dmitrienko <dmitrienko@sl.iae.nsk.su>
2022-03-11 23:13:10 +0700

    Signed-off-by: Mikhail Y. Dmitrienko <dmitrienko@sl.iae.nsk.su>

--- idwt_benchmark.py
+++ idwt_benchmark.py
@@ -14,7 +14,9 @@ def main():
     tets_len = 100
     for i in range(100):
         times.append(
-            decode("..\jxs_cpp\cmake-build-release\jxs_decoder.exe bitstream_dataset/1920x1080/3.jxs tmp/3.ppm"))
+            # decode("..\jxs_cpp\cmake-build-release\jxs_decoder.exe bitstream_dataset/3840x2160/3.jxs tmp/3.ppm"))
+            # decode("..\jxs_cpp\cmake-build-release\jxs_decoder.exe bitstream_dataset/1920x1080/3.jxs tmp/3.ppm"))
+            decode("D:\dmitrienkomy\cpp\jxs\jxs cpp ref\jxs_cpp\out\\build\\x64-Release\jxs_decoder.exe bitstream_dataset/1920x1080/3.jxs tmp/3.ppm"))
     mean = sum(times) / tets_len
     stdev = statistics.stdev(times)
     print(f"mean {mean} \t stddev {stdev}")
